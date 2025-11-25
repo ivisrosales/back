18bbc739edba1d846540afda2a8b27fc4bc47562
Ivis Rosales Vargas <ivisrosalesvargas@gmail.com>
2025-11-25 16:03:21 -0400

    quinto commit

--- app.js
+++ app.js
@@ -8,11 +8,13 @@ app.use(morgan('dev')); // Logging [cite: 1122]
 app.use(express.json()); // Body Parser [cite: 1129]
  
 // TODO: Cargar Rutas (lo haremos en Clase 2)
+const productRoutes = require('./src/presentation/routes/product.routes');
+app.use('/api/v1/products', productRoutes);
  
 // Healthcheck Endpoint (para probar)
 app.get('/api/v1/healthcheck', (req, res) => {
    res.status(200).json({ status: 'ok', timestamp: new Date() });
 });
  
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
